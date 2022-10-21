f0972d04dacd7a3e285c2d11f92d12c02128354a
marius93rm <marius93rm@gmail.com>
2022-10-21 10:26:04 +0200

    updoated

--- App.js
+++ App.js
@@ -1,36 +1,71 @@
 import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, TextInput, Text, Image, TouchableOpacity } from 'react-native';
-
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Text,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  TouchableHighlight,
+} from 'react-native';
 
 export default function App() {
   const [text, setText] = useState('');
   const [count, setCount] = useState(0);
 
+  const image = require('./assets/pattern.jpg');
+
   return (
     <View style={styles.container}>
-      <Image
+      <ImageBackground
+        source={image}
         style={styles.image}
-        source={ require('./assets/icon.png') } 
-      />
-      <TextInput
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1, padding: 5, width: 200 }}
-        placeholder="You can type in me"
-        onChangeText={
-          (text) => {
-            setText(text);
-          }
-        }
-        defaultValue={text}
-      />
+        resizeMode="cover"
+      >
+        <View style={styles.centro}>
+          <TextInput
+            style={{
+              height: 40,
+              borderColor: 'gray',
+              borderWidth: 1,
+              padding: 5,
+              width: 200,
+            }}
+            placeholder="You can type in me"
+            onChangeText={(text) => {
+              setText(text);
+            }}
+            defaultValue={text}
+          />
 
-      <Text style={{ padding: 10, fontSize: 42 }}>
-        {text}
-      </Text>
+          <Text style={{ padding: 10, fontSize: 42 }}>{text}</Text>
+          <Text style={{ padding: 10, fontSize: 42 }}>{count}</Text>
 
-      <TouchableOpacity
-        style={styles.button}
-      <StatusBar style="auto" />
+          <TouchableOpacity
+            style={styles.button}
+            activeOpacity={0.9}
+            onPress={() => {
+              setCount(count + 1);
+            }}>
+            <Text style={styles.buttonText}>Click me!</Text>
+          </TouchableOpacity>
+
+          <TouchableHighlight
+            style={styles.button}
+            underlayColor="FFF"
+            onPress={() => {
+              setCount(count + 1);
+            }}>
+            <Text style={styles.buttonText}>Click me!</Text>
+          </TouchableHighlight>
+
+        </View>
+
+
+
+
+      </ImageBackground>
     </View>
   );
 }
@@ -39,18 +74,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
   image: {
     width: 200,
-    height: 200
+    height: 200,
   },
   button: {
     padding: 40,
     borderRadius: 4,
     borderWidth: 0.5,
     borderColor: '#d6d7da',
-    backgroundColor: 'blue'
-  }
+    backgroundColor: '#F88',
+  },
+  buttonText: {
+    color: 'white',
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  centro: {
+    alignItems: "center",
+  },
 });
